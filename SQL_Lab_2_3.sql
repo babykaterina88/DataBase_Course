6c9c71d49471d154f52f3931efda24a900de500e
babykaterina88 <153901741+babykaterina88@users.noreply.github.com>
2023-12-27 00:26:41 +0300

    Update SQL_Lab_2_3.sql

--- SQL_Lab_2_3.sql
+++ SQL_Lab_2_3.sql
@@ -1,2 +1,6 @@
 -- Задание 3: Найти среднее число часов выходных (VacationHours) для разных полов из таблицы HumanResources.Employee.
-SELECT "Gender", AVG("VacationHours") AS "AverageVacationHours" FROM "HumanResources"."Employee" GROUP BY "Gender";
+SELECT "Gender", AVG("VacationHours") AS "AverageVacationHours"
+FROM "HumanResources"."Employee"
+WHERE "VacationHours" > 0
+GROUP BY "Gender";
+
