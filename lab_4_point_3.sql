df5ef0ff9ad8052e1ef223e62311197d3874bdd3
Екатерина Минаева <153901741+babykaterina88@users.noreply.github.com>
2024-01-04 21:28:40 +0300

    Update lab_4_point_3.sql

--- lab_4_point_3.sql
+++ lab_4_point_3.sql
@@ -5,17 +5,23 @@
   Показать поля [Name], Style и Color.
 */
 SELECT 
-    P."Name", 
-    P."Style", 
-    P."Color" 
+    "Name", 
+    "Style", 
+    "Color"
 FROM 
-    "Production"."Product" P
+    "Production"."Product" AS P
 WHERE 
-    P."Style" IS NOT NULL 
-    AND P."Color" IS NOT NULL
-    AND (SELECT COUNT(DISTINCT P2."Style") 
-         FROM "Production"."Product" P2
-         WHERE P2."ProductID" = P."ProductID") = 1
-    AND (SELECT COUNT(DISTINCT P2."Color") 
-         FROM "Production"."Product" P2
-         WHERE P2."ProductID" = P."ProductID") = 1;
+    EXISTS (
+        SELECT 1
+        FROM "Production"."Product" AS P2
+        WHERE P2."Style" = P."Style" AND P2."ProductID" <> P."ProductID"
+        GROUP BY P2."Style"
+        HAVING COUNT(DISTINCT P2."Color") = 1
+    )
+    AND EXISTS (
+        SELECT 1
+        FROM "Production"."Product" AS P2
+        WHERE P2."Color" = P."Color" AND P2."ProductID" <> P."ProductID"
+        GROUP BY P2."Color"
+        HAVING COUNT(DISTINCT P2."Style") = 1
+    );
